396c3253fa356ca1b5549e7db969a51ab21c42c4
Faustino <faustino@MEXL007870.local>
2023-03-03 12:24:32 -0600

    Pie and line chart created

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import Form from "./scenes/form";
 import Calendar from "./scenes/calendar";
 import Faq from "./scenes/faq";
 import Bar from "./scenes/bar";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
+import Pie from "./scenes/pie";
+import Line from "./scenes/line";
 // import Geography from "./scenes/geography";
 
 
@@ -36,8 +36,8 @@ function App() {
               <Route path="/calendar" element={<Calendar/>} />
               <Route path="/faq" element={<Faq/>} />
               <Route path="/bar" element={<Bar/>} />
-              {/* <Route path="/line" element={<Line/>} /> */}
-              {/* <Route path="/pie" element={<Pie/>} /> */}
+              <Route path="/pie" element={<Pie/>} />
+              <Route path="/line" element={<Line/>} />
               {/* <Route path="/geography" element={<Geography/>} /> */}
             </Routes>
           </main>
