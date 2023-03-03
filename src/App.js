912d7cd7c6bffd0b10025c6b5a444d544d4bf957
Faustino <faustino@MEXL007870.local>
2023-03-03 11:48:56 -0600

    FAQ & Bar chart created

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
 import Calendar from "./scenes/calendar";
-// import Bar from "./scenes/bar";
+import Faq from "./scenes/faq";
+import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-// import Faq from "./scenes/faq";
 // import Geography from "./scenes/geography";
 
 
@@ -34,10 +34,10 @@ function App() {
               <Route path="/invoices" element={<Invoices/>} />
               <Route path="/form" element={<Form/>} />
               <Route path="/calendar" element={<Calendar/>} />
-              {/* <Route path="/bar" element={<Bar/>} /> */}
+              <Route path="/faq" element={<Faq/>} />
+              <Route path="/bar" element={<Bar/>} />
               {/* <Route path="/line" element={<Line/>} /> */}
               {/* <Route path="/pie" element={<Pie/>} /> */}
-              {/* <Route path="/faq" element={<Faq/>} /> */}
               {/* <Route path="/geography" element={<Geography/>} /> */}
             </Routes>
           </main>
